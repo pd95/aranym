3c71f2f1678418e92cec7e70403ad4e067affe4f
Philipp <pd95@users.noreply.github.com>
2023-07-02 17:42:30 +0200

    Use SDL2 from dependencies.zip if not overriden

--- .github/install_prereq_macos.sh
+++ .github/install_prereq_macos.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-SDL2_VERSION=2.28.1
+SDL2_VERSION=
 DEPENDENCIES_DATE=2023-07-02
 
 mkdir tmp
@@ -8,17 +8,20 @@ cd tmp
 
 brew install autoconf automake
 
-# SDL2
-curl -s --get https://www.libsdl.org/release/SDL2-$SDL2_VERSION.dmg --output SDL2.dmg
-mkdir sdltmp
-hdiutil attach -mountpoint sdltmp -readonly SDL2.dmg
-cp -R sdltmp/SDL2.framework ../src/Unix/MacOSX/SDL2.Framework
-hdiutil detach sdltmp
-
+# Download additional dependencies (libjpeg, libgmp, libgmpxx, libmpfr) as universal binaries (not available through homebrew)
 curl -s --get https://xn--donz-epa.ch/atari/software/macaranym/Dependencies-$DEPENDENCIES_DATE.zip --output dependencies.zip
 unzip -o dependencies.zip -d ../src/Unix/MacOSX/
 mv external ../src/Unix/MacOSX/external
 cd ..
+
+# Optionally add specific SDL2 version
+if [ "x$SDL2_VERSION" != "x" ]; then
+    curl -s --get https://www.libsdl.org/release/SDL2-$SDL2_VERSION.dmg --output SDL2.dmg
+    mkdir sdltmp
+    hdiutil attach -mountpoint sdltmp -readonly SDL2.dmg
+    cp -R sdltmp/SDL2.framework ../src/Unix/MacOSX/SDL2.Framework
+    hdiutil detach sdltmp
+fi
 
 # cleanup tmp
 rm -rf tmp
