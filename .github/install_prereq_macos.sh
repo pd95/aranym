56139c35d653620b62f8d76c430e8ffe6d52c2d3
Philipp <pd95@users.noreply.github.com>
2023-07-02 23:56:44 +0200

    Add GitHub action to create macOS build  (#5)
    * Add GitHub action for macOS build
    * Create install_prereq_macos.sh to fetch dependencies (homebrew and self hosted which include arm64 and x86_64 binaries)
    * Sign apps and disk image using private certificate
    * Notarize disk image before uploading artifact after build
    * Allow manual triggering of action

---
+++ .github/install_prereq_macos.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+SDL2_VERSION=
+DEPENDENCIES_DATE=2023-07-02
+
+mkdir tmp
+cd tmp
+
+brew install autoconf automake
+
+# Download additional dependencies (libjpeg, libgmp, libgmpxx, libmpfr) as universal binaries (not available through homebrew)
+curl -s --get https://xn--donz-epa.ch/atari/software/macaranym/Dependencies-$DEPENDENCIES_DATE.zip --output dependencies.zip
+unzip -o dependencies.zip -d ../src/Unix/MacOSX/
+mv external ../src/Unix/MacOSX/external
+cd ..
+
+# Optionally add specific SDL2 version
+if [ "x$SDL2_VERSION" != "x" ]; then
+    curl -s --get https://www.libsdl.org/release/SDL2-$SDL2_VERSION.dmg --output SDL2.dmg
+    mkdir sdltmp
+    hdiutil attach -mountpoint sdltmp -readonly SDL2.dmg
+    cp -R sdltmp/SDL2.framework ../src/Unix/MacOSX/SDL2.Framework
+    hdiutil detach sdltmp
+fi
+
+# cleanup tmp
+rm -rf tmp
